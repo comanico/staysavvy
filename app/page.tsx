ae073055ac701fbbf2d8ff9f136f36a4178c2c05
comanico <Alexandru-Nicol.Comaniciu@morganstanley.com>
2025-04-24 04:34:45 -0400

    Updated app/page.tsx to respect NextJS 15 structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,16 @@ import { getHotels } from "@/actions/getHotels";
 import HotelList from "@/components/hotel/HotelList";
 
 interface HomeProps {
-  searchParams: {
+  searchParams: Promise<{
     title: string;
     country: string;
     state: string;
     city: string;
-  };
+  }>;
 }
 
 export default async function Home({ searchParams }: HomeProps) {
-  const hotels = await getHotels(searchParams);
+  const hotels = await getHotels(await searchParams);
   
   if(!hotels) return <div>No hotels found...</div>;
 
