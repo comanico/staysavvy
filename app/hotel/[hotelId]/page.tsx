c600b61ae9c10d58fca61d6c5917985a1be7a46b
Alexandru Comaniciu <comaniciu.nicolae@gmail.com>
2025-04-30 18:09:24 +0300

    Added layer of security and fixed hydration error

--- app/hotel/[hotelId]/page.tsx
+++ app/hotel/[hotelId]/page.tsx
@@ -12,8 +12,13 @@ const Hotel = async ({ params }: HotelPageProps) => {
   const param = await params;
   const hotel = await getHotelById(param.hotelId);
   const { userId } = await auth();
+  const whitelistedUserIds = (process.env.WHITELISTED_USERS || "").split(',')
 
   if (!userId) return <div>Not authenticated...</div>;
+
+  if (!whitelistedUserIds.includes(userId)) {
+    return <div>Access denied...</div>;
+  }
 
   if (hotel && hotel.userId !== userId) return <div>Access denied...</div>;
 
